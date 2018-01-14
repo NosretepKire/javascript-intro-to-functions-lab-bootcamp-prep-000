b670dd5ddeb8f3d77eb38b1c0e8ad252de9b7957
Erik Peterson <erikeh1@gmail.com>
2018-01-14 05:16:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ function logWhisper(string) { console.log(string .toLowerCase())}
 function sayHiToGrandma(string) { if(string === string.toLowerCase())
   return "I can't hear you!"
   if(string === string.toUpperCase())
-return"YES INDEED!" }
+return"YES INDEED!"
+ if(string === "I love you, Grandma.")
+  return "I love you, too."
+}
