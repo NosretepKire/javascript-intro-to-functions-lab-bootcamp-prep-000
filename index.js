62d287b7adf679d5494eaf729944be13c14256d6
Erik Peterson <erikeh1@gmail.com>
2018-01-14 05:14:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ function whisper(string) { return string .toLowerCase()}
 function logShout(string) { console.log(string .toUpperCase())}
 function logWhisper(string) { console.log(string .toLowerCase())}
 function sayHiToGrandma(string) { if(string === string.toLowerCase())
-  return "I can't hear you!" if(string === string.toUpperCase())
+  return "I can't hear you!"
+  if(string === string.toUpperCase())
 return"YES INDEED!" }
